ce71a42b9eb63c1d25c557d0aa524693f5977b5c
王国强 <moonpython@163.com>
2024-03-22 16:55:58 +0800

    LeNet

--- common/d2l.py
+++ common/d2l.py
@@ -9,6 +9,7 @@ import time
 from IPython import display
 import torch
 from torch.utils import data
+from torch import nn
 import torchvision
 from torchvision import transforms
 from matplotlib_inline import backend_inline
@@ -269,6 +270,32 @@ def evaluate_accuracy(net, data_iter):
     return metric[0] / metric[1]
 
 
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    """
+    使用GPU计算模型在数据集上的精度
+    :param net: 模型
+    :param data_iter: 数据
+    :param device: 设备
+    :return:
+    """
+    if isinstance(net, nn.Module):
+        net.eval()  # 设置为评估模式
+        if not device:
+            device = next(iter(net.parameters())).device
+    # 正确预测的数量，总预测的数量
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                # BERT微调所需的（之后将介绍）
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
 def evaluate_loss(net, data_iter, loss):  # @save
     """
     评估给定数据集上模型的损失
@@ -377,6 +404,46 @@ def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
     assert train_loss < 0.5, train_loss
     assert 0.7 < train_acc < 1, train_acc
     assert 0.8 < test_acc < 1, test_acc
+
+
+def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
+    """用GPU训练模型(在第六章定义)"""
+
+    def init_weights(m):
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            nn.init.xavier_uniform_(m.weight)
+
+    net.apply(init_weights)
+    print('正在 {} 上训练。。。'.format(device))
+    net.to(device)
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+    loss = nn.CrossEntropyLoss()
+    animator = Animator(xlabel='迭代周期', xlim=[1, num_epochs], legend=['训练损失', '训练精度', '测试精度'])
+    timer, num_batches = Timer(), len(train_iter)
+    for epoch in range(num_epochs):
+        # 训练损失之和，训练准确率之和，样本数
+        metric = Accumulator(3)
+        net.train()
+        for i, (X, y) in enumerate(train_iter):
+            timer.start()
+            optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(l * X.shape[0], accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            train_l = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches,
+                             (train_l, train_acc, None))
+        test_acc = evaluate_accuracy_gpu(net, test_iter)
+        animator.add(epoch + 1, (None, None, test_acc))
+    print(f'损失 {train_l:.3f}, 训练精度 {train_acc:.3f},  测试精度 {test_acc:.3f}')
+    print(f'{metric[2] * num_epochs / timer.sum():.1f} 个样本/秒 在 {str(device)} 上')
 
 
 def predict_ch3(net, test_iter, n=10):
--- debug.py
+++ debug.py
@@ -33,6 +33,3 @@ def comp_conv2d(conv2d, X):
 conv2d = nn.Conv2d(1, 1, kernel_size=3, padding=1)
 X = torch.rand(size=(8, 8))
 print(comp_conv2d(conv2d, X))
-
-
-nn.MaxPool2d()
