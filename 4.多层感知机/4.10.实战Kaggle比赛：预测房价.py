0227736cb9030821543ab73e8256f035fab50816
王国强 <moonpython@163.com>
2024-12-10 13:56:30 +0800

    4.10.Kggle预测房价

--- 4.多层感知机/4.10.实战Kaggle比赛：预测房价.py
+++ 4.多层感知机/4.10.实战Kaggle比赛：预测房价.py
@@ -4,72 +4,124 @@
 @Time    : 2024/12/5 16:40
 @Desc    : 
 """
-import hashlib
-import os
-import tarfile
-import zipfile
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn
 
-import requests
+import d2l
 
-# 1. 下载和缓存数据集
+train_data = pd.read_csv(d2l.download('kaggle_house_train'))
+test_data = pd.read_csv(d2l.download('kaggle_house_test'))
 
-DATA_HUB = dict()
-DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+# 第一个特征是ID，不携带任何用于预测的信息，在将数据提供给模型之前，我们将其从数据集中删除
+all_features = pd.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
 
-
-def download(name, cache_dir=os.path.join('..', 'data')):
-    """下载一个DATA_HUB中的文件，返回本地文件名"""
-    assert name in DATA_HUB, f"{name} 不存在于 {DATA_HUB}"
-    url, sha1_hash = DATA_HUB[name]
-    os.makedirs(cache_dir, exist_ok=True)
-    fname = os.path.join(cache_dir, url.split('/')[-1])
-    if os.path.exists(fname):
-        sha1 = hashlib.sha1()
-        with open(fname, 'rb') as f:
-            while True:
-                data = f.read(1048576)
-                if not data:
-                    break
-                sha1.update(data)
-        if sha1_hash == sha1.hexdigest():
-            return fname
-    print(f"正在从{url}下载{fname}")
-    r = requests.get(url, stream=True, verify=True)
-    with open(fname, 'wb') as f:
-        f.write(r.content)
-    return fname
-
-
-def download_extract(name, folder=None):
-    """下载并解压zip/tar文件"""
-    fname = download(name)
-    base_dir = os.path.dirname(fname)
-    data_dir, ext = os.path.splitext(fname)
-    if ext == '.zip':
-        fp = zipfile.ZipFile(fname, 'r')
-    elif ext in ('.tar', '.gz'):
-        fp = tarfile.open(fname, 'r')
-    else:
-        assert False, '只有zip/tar文件可以被解压缩'
-    fp.extract(base_dir)
-    return os.path.join(base_dir, folder) if folder else data_dir
-
-
-def download_all():
-    """下载DATA_HUB中的所有文件"""
-    for name in DATA_HUB:
-        download(name)
-
-
-DATA_HUB['kaggle_house_train'] = (
-    DATA_URL + 'kaggle_house_pred_train.csv',
-    '585e9cc93e70b39160e7921475f9bcd7d31219ce'
+# 若无法获得测试数据，则可根据训练数据计算均值和标准差
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+all_features[numeric_features] = all_features[numeric_features].apply(
+    lambda x: (x - x.mean()) / x.std()
 )
+# 在标准化数据之后，所有均值为0，因此我们可以将缺失值设置为0
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
 
-DATA_HUB['kaggle_house_test'] = (
-    DATA_URL + 'kaggle_house_pred_test.csv',
-    'fa19780a7b011d9b009e8bff8e99922a8ee2eb90'
-)
+# “Dummy_na=True”将“na”（缺失值）视为有效的特征值，并为其创建指示符特征
+all_features = pd.get_dummies(all_features, dummy_na=True)
+
+# 我们可以 从pandas格式中提取NumPy格式，并将其转换为张量表示用于训练
+n_train = train_data.shape[0]
+train_features = torch.tensor(all_features[:n_train].values.astype(np.float32), dtype=torch.float32)
+test_features = torch.tensor(all_features[n_train:].values.astype(np.float32), dtype=torch.float32)
+train_labels = torch.tensor(train_data.SalePrice.values.reshape(-1, 1), dtype=torch.float32)
+
+loss = nn.MSELoss()  # 均方误差损失函数，reduction参数默认是'mean'
+in_features = train_features.shape[1]
+
+
+def get_net():
+    return nn.Sequential(nn.Linear(in_features, 1))
+
+
+def log_rmse(net, features, labels):
+    # 为了在取对数时进一步稳定该值，将输出值裁剪到1到正无穷之间
+    clipped_preds = torch.clamp(net(features), 1, float('inf'))
+    rmse = torch.sqrt(loss(torch.log(clipped_preds), torch.log(labels)))
+    return rmse.item()
+
+
+def train(net, train_features, train_labels, test_features, test_labels,
+          num_epochs, learning_rate, weight_decay, batch_size):
+    train_ls, test_ls = [], []
+    train_iter = d2l.load_array((train_features, train_labels), batch_size)
+    # 这里使用的是Adam优化算法
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            optimizer.zero_grad()
+            l = loss(net(X), y)
+            l.backward()
+            optimizer.step()
+        train_ls.append(log_rmse(net, train_features, train_labels))
+        if test_labels is not None:
+            test_ls.append(log_rmse(net, test_features, test_labels))
+    return train_ls, test_ls
+
+
+def get_k_fold_data(k, i, X, y):
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train, X_valid, y_valid = None, None, None, None
+    for j in range(k):
+        idx = slice(j * fold_size, j * fold_size + fold_size)
+        X_part, y_part = X[idx, :], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = torch.cat([X_train, X_part], 0)
+            y_train = torch.cat([y_train, y_part], 0)
+    return X_train, y_train, X_valid, y_valid
+
+
+def k_fold(k, X_train, y_train, num_epochs, learning_rate, weight_decay, batch_size):
+    train_l_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        net = get_net()
+        train_ls, valid_ls = train(net, *data, num_epochs, learning_rate, weight_decay, batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        if i == 0:
+            d2l.plot(list(range(1, num_epochs + 1)), [train_ls, valid_ls], xlabel="训练轮数",
+                     ylabel="对数均方根误差", xlim=[1, num_epochs], legend=["训练集", "验证集"], yscale='log')
+        print(f'{i + 1}折，训练 log rmse = {float(train_ls[-1]):f}, '
+              f'验证 log rmse = {float(valid_ls[-1]):f}')
+        break
+    return train_l_sum / k, valid_l_sum / k
+
+
+def train_and_pred(train_features, test_features, train_labels, test_data,
+                   num_epochs, lr, weight_decay, batch_size):
+    net = get_net()
+    train_ls, _ = train(net, train_features, train_labels, None, None,
+                        num_epochs, lr, weight_decay, batch_size)
+    d2l.plot(list(range(1, num_epochs + 1)), [train_ls], xlabel="训练轮数",
+             ylabel="log rmse", xlim=[1, num_epochs], yscale='log')
+    print(f'训练log rmse：{float(train_ls[-1]):f}')
+    # 将网络应用于测试集
+    preds = net(test_features).detach().numpy()
+    # 将其重新格式化以导出到Kaggle
+    test_data['SalePrice'] = pd.Series(preds.reshape(1, -1)[0])
+    submission = pd.concat([test_data['Id'], test_data['SalePrice']], axis=1)
+    submission.to_csv('submission.csv', index=False)
+
 
 if __name__ == '__main__':
-    download_all()
+    k, num_epochs, lr, weight_decay, batch_size = 5, 1500, 1, 0, 64
+    # train_l, valid_l = k_fold(k, train_features, train_labels, num_epochs,
+    #                           lr, weight_decay, batch_size)
+    # print(f'{k}-折验证: 平均训练 log rmse: {float(train_l):f}, '
+    #       f'平均验证 log rmse: {float(valid_l):f}')
+    train_and_pred(train_features, test_features, train_labels, test_data,
+                   num_epochs, lr, weight_decay, batch_size)
--- 4.多层感知机/d2l.py
+++ 4.多层感知机/d2l.py
@@ -4,11 +4,15 @@
 @Time    : 2024/10/24 10:02
 @Desc    : 
 """
+import hashlib
 import sys
 import os
+import tarfile
 import time
+import zipfile
 
 import numpy as np
+import requests
 import torchvision.datasets
 from matplotlib import pyplot as plt
 from matplotlib_inline import backend_inline
@@ -57,7 +61,7 @@ def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
 # 通过这三个用于图形配置的函数，定义一个plot函数来简洁地绘制多条曲线，因为我们需要在整个书中可视化许多曲线。
 def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None,
          ylim=None, xscale='linear', yscale='linear',
-         fmts=('-', 'm--', 'g-.', 'r:'), figsize=(3.5, 2.5), axes=None):
+         fmts=('-', 'm--', 'g-.', 'r:'), figsize=(5.5, 3.5), axes=None):
     """绘制数据点"""
     if legend is None:
         legend = []
@@ -352,3 +356,62 @@ def evaluate_loss(net, data_iter, loss):
         l = loss(out, y)
         metric.add(l.sum(), l.numel())
     return metric[0] / metric[1]
+
+
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+
+
+def download(name, cache_dir=os.path.join('..', 'data')):
+    """下载一个DATA_HUB中的文件，返回本地文件名"""
+    assert name in DATA_HUB, f"{name} 不存在于 {DATA_HUB}"
+    url, sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir, exist_ok=True)
+    fname = os.path.join(cache_dir, url.split('/')[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1_hash == sha1.hexdigest():
+            return fname
+    print(f"正在从{url}下载{fname}")
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
+
+
+def download_extract(name, folder=None):
+    """下载并解压zip/tar文件"""
+    fname = download(name)
+    base_dir = os.path.dirname(fname)
+    data_dir, ext = os.path.splitext(fname)
+    if ext == '.zip':
+        fp = zipfile.ZipFile(fname, 'r')
+    elif ext in ('.tar', '.gz'):
+        fp = tarfile.open(fname, 'r')
+    else:
+        assert False, '只有zip/tar文件可以被解压缩'
+    fp.extract(base_dir)
+    return os.path.join(base_dir, folder) if folder else data_dir
+
+
+def download_all():
+    """下载DATA_HUB中的所有文件"""
+    for name in DATA_HUB:
+        download(name)
+
+
+DATA_HUB['kaggle_house_train'] = (
+    DATA_URL + 'kaggle_house_pred_train.csv',
+    '585e9cc93e70b39160e7921475f9bcd7d31219ce'
+)
+
+DATA_HUB['kaggle_house_test'] = (
+    DATA_URL + 'kaggle_house_pred_test.csv',
+    'fa19780a7b011d9b009e8bff8e99922a8ee2eb90'
+)
