387ab9bcac5e0048b656025063e5b4bc205dd9a4
王国强 <moonpython@163.com>
2025-03-13 16:05:22 +0800

    编码器-解码器架构

--- 9.现代循环神经网络/d2l.py
+++ 9.现代循环神经网络/d2l.py
@@ -1094,4 +1094,42 @@ def load_data_nmt(batch_size, num_steps, num_examples=600):
     return data_iter, src_vocab, tgt_vocab
 
 
+class Encoder(nn.Module):
+    """编码器-解码器架构的基本编码器接口"""
 
+    def __init__(self, **kwargs):
+        super(Encoder, self).__init__(**kwargs)
+
+    def forward(self, X, *rgs):
+        # X 为输入序列（长度可变）
+        # 此处应实现编码逻辑，将 X 转换为固定形状的编码状态
+        raise NotImplementedError
+
+
+class Decoder(nn.Module):
+    """编码器-解码器架构的基本解码器接口"""
+
+    def __init__(self, **kwargs):
+        super(Decoder, self).__init__(**kwargs)
+
+    def init_state(self, enc_outputs, *args):
+        raise NotImplementedError
+
+    def forward(self, X, state):
+        raise NotImplementedError
+
+
+class EncoderDecoder(nn.Module):
+    """编码器-解码器架构的基类"""
+
+    def __init__(self, encoder, decoder, **kwargs):
+        super(EncoderDecoder, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, enc_X, dec_X, *args):
+        # enc_X：输入序列
+        # dec_X：解码器接收的输入（例如目标序列的部分）
+        enc_outputs = self.encoder(enc_X, *args)
+        dec_state = self.decoder.init_state(enc_outputs, *args)
+        return self.decoder(dec_X, dec_state)
