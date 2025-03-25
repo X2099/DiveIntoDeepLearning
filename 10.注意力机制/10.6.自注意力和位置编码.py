4631fd4fd2161964396809bc5257df0f5915a708
王国强 <moonpython@163.com>
2025-03-25 17:20:32 +0800

    自注意力

---
+++ 10.注意力机制/10.6.自注意力和位置编码.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : 10.6.自注意力和位置编码.py
+@Time    : 2025/3/24 16:53
+@Desc    : 
+"""
+import torch
+import d2l
+
+num_hiddens, num_heads = 100, 5
+attention = d2l.MultiHeadAttention(num_hiddens, num_hiddens, num_hiddens,
+                                   num_hiddens, num_heads, 0.5)
+attention.eval()
+
+# print(attention)
+"""输出：
+
+MultiHeadAttention(
+  (attention): DotProductAttention(
+    (dropout): Dropout(p=0.5, inplace=False)
+  )
+  (W_q): Linear(in_features=100, out_features=100, bias=False)
+  (W_k): Linear(in_features=100, out_features=100, bias=False)
+  (W_v): Linear(in_features=100, out_features=100, bias=False)
+  (W_o): Linear(in_features=100, out_features=100, bias=False)
+)
+"""
+
+batch_size, num_queries, valid_lens = 2, 4, torch.tensor([3, 2])
+X = torch.ones((batch_size, num_queries, num_hiddens))
+
+# print(attention(X, X, X, valid_lens).shape)
+# 输出：torch.Size([2, 4, 100])
+
+
+encoding_dim, num_steps = 32, 60
+pos_encoding = d2l.PositionalEncoding(encoding_dim, 0)
+pos_encoding.eval()
+X = pos_encoding(torch.zeros((1, num_steps, encoding_dim)))
+P = pos_encoding.P[:, :X.shape[1], :]
+# d2l.plot(torch.arange(num_steps), P[0, :, 6:10].T, xlabel='Row (position)',
+#          figsize=(6.18, 3.82), legend=["Col %d" % d for d in torch.arange(6, 10)])
+
+for i in range(8):
+    print(f'{i}的二进制是：{i:>03b}')
+
+P = P[0, :, :].unsqueeze(0).unsqueeze(0)
+d2l.show_heatmaps(P, xlabel='Column (encoding dimension)',
+                  ylabel='Row (position)', figsize=(3.82, 6.18), cmap='Blues')
+d2l.plt.show()
--- 10.注意力机制/d2l.py
+++ 10.注意力机制/d2l.py
@@ -1528,3 +1528,22 @@ class MultiHeadAttention(nn.Module):
         # output_concat的形状:(batch_size, 查询的个数, num_hiddens)
         output_concat = transpose_output(output, self.num_heads)
         return self.W_o(output_concat)
+
+
+class PositionalEncoding(nn.Module):
+    """位置编码"""
+
+    def __init__(self, num_hiddens, dropout, max_len=1000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        # 创建一个足够长的P
+        self.P = torch.zeros((1, max_len, num_hiddens))
+        X = torch.arange(max_len, dtype=torch.float32).reshape(
+            -1, 1) / torch.pow(10000, torch.arange(
+            0, num_hiddens, 2, dtype=torch.float32) / num_hiddens)
+        self.P[:, :, 0::2] = torch.sin(X)
+        self.P[:, :, 1::2] = torch.cos(X)
+
+    def forward(self, X):
+        X = X + self.P[:, :X.shape[1], :].to(X.device)
+        return self.dropout(X)
